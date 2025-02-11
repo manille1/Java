bb08e82095278e3368f90d95091fbe64606c0917
Manille Desseaux <manille024@gmail.com>
2025-02-11 09:54:20 +0100

    exo1

---
+++ Attributs/Exercices/src/Exo1.java
@@ -0,0 +1,15 @@
+public class App {
+    public class Book {
+        private String title;
+
+        // Getter pour récupérer le title
+        public String getTitle() {
+            return title;
+        }
+
+        // Setter pour modifier le title
+        public void setTitle(String title) {
+            this.title = title;
+        }
+    }
+}
